01890b5c4994f743de3c042f2254fcdc16790784
niklas <niklas.gerlach@outlook.com>
2025-08-12 12:03:20 +0200

    bugfix: handle unnecessary arg passing

--- src/models/ts_transformer.py
+++ src/models/ts_transformer.py
@@ -168,7 +168,8 @@ class TransformerBatchNormEncoderLayer(nn.modules.Module):
         super(TransformerBatchNormEncoderLayer, self).__setstate__(state)
 
     def forward(self, src: Tensor, src_mask: Optional[Tensor] = None,
-                src_key_padding_mask: Optional[Tensor] = None) -> Tensor:
+                src_key_padding_mask: Optional[Tensor] = None,
+                is_causal: Optional[bool] = False) -> Tensor:
         r"""Pass the input through the encoder layer.
 
         Args:
